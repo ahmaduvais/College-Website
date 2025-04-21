f81d70db305dbb5e0739ae37d8d5ca2653312c9d
ahmaduvais <uvais.amualigarh@gmail.com>
2025-04-22 00:00:14 +0530

    -21/04/2025

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/College-Website">
+    <BrowserRouter basename="/College-Website/">
       <App />
     </BrowserRouter>
   </StrictMode>,
