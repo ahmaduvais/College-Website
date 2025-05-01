e0b72888bd5a389f284831917fb211c52b9457db
ahmaduvais <uvais.amualigarh@gmail.com>
2025-05-02 03:13:42 +0530

    default

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-    {/* <BrowserRouter basename="/College-Website/"> */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/College-Website/",
+  base:"/",
   build:{
     outDir:'dist',
   }
